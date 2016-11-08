a65fc4e2f3f8fda81bde52b66ac7d15bf4df4a5a
thedevsaddam <thedevsaddam@gmail.com>
2016-11-08 19:03:27 +0600

    Initial commit

---
+++ config.py
@@ -0,0 +1,11 @@
+# Configuration
+access_token = "YOUR ACCESS TOKEN"
+user_name = "USER NAME WILL BE DISPLAYED"
+channel = "CHANNEL NAME TO SEND THE REMINDER"
+prayer_times = {
+    "Fajr": "17:22",
+    "Dhuhr": "13:20",
+    "Asr": "18:15",
+    "Maghrib": "17:30",
+    "Isha": "18:52"
+}
---
+++ prayer.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python
+import requests
+import datetime
+from config import *
+
+BASE_URL = "https://slack.com/api/chat.postMessage"
+message = "prayer will be held within 5 minutes"
+now = datetime.datetime.now()
+
+
+def is_prayer_available(hour, minute):
+    for prayer_name, prayer_time in prayer_times.items():
+        prayer_time_ = (str(prayer_time)).split(":")
+        prayer_time_minutes = (int(prayer_time_[0]) * 60) + int(prayer_time_[1])
+        current_time_minutes = int(hour * 60) + int(minute)
+        if current_time_minutes in range((prayer_time_minutes - 5), prayer_time_minutes + 1):
+            return prayer_name
+
+    return None
+
+
+if is_prayer_available(now.hour, now.minute):
+    params = {
+        "text": "*" + is_prayer_available(now.hour, now.minute) + "* " + message,
+        "channel": channel,
+        "token": access_token,
+        "username": user_name
+    }
+    requests.get(BASE_URL, params)
